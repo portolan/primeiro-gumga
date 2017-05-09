6f1bea3304f0313df3f432f89d75452a70f577c5
portolan <hort_e@hotmail.com>
2017-05-09 14:28:18 -0300

    teste

--- primeiro-presentation/src/main/webapp/app/modules/turma/controllers/TurmaListController.js
+++ primeiro-presentation/src/main/webapp/app/modules/turma/controllers/TurmaListController.js
@@ -1,29 +1,34 @@
 define([], function() {
 
-  TurmaListController.$inject = ['$scope', 'TurmaService', 'gumgaController'];
+    TurmaListController.$inject = ['$scope', 'TurmaService', 'gumgaController'];
 
-  function TurmaListController($scope, TurmaService, gumgaController) {
+    function TurmaListController($scope, TurmaService, gumgaController) {
 
-    gumgaController.createRestMethods($scope, TurmaService, 'turma');
+        gumgaController.createRestMethods($scope, TurmaService, 'turma');
 
-    TurmaService.resetDefaultState();
-    $scope.turma.execute('get');
+        TurmaService.resetDefaultState();
+        $scope.turma.execute('get');
 
-    $scope.tableConfig = {
-      columns: 'codigo ,button',
-      checkbox: true,
-      columnsConfig: [{
-        name: 'codigo',
-        title: '<span gumga-translate-tag="turma.codigo"> codigo </span>',
-        content: '{{$value.codigo }}',
-        sortField: 'codigo'
-      }, {
-        name: 'button',
-        title: ' ',
-        content: '<span class="pull-right"><a class="btn btn-primary btn-sm" ui-sref="turma.edit({id: {{$value.id}} })"><i class="glyphicon glyphicon-pencil"></i></a></span>'
-      }]
+
+        $scope.turma.on('deleteSuccess', function() {
+            $scope.turma.execute('get')
+        })
+
+        $scope.tableConfig = {
+            columns: 'codigo ,button',
+            checkbox: true,
+            columnsConfig: [{
+                name: 'codigo',
+                title: '<span gumga-translate-tag="turma.codigo"> codigo </span>',
+                content: '{{$value.codigo }}',
+                sortField: 'codigo'
+            }, {
+                name: 'button',
+                title: ' ',
+                content: '<span class="pull-right"><a class="btn btn-primary btn-sm" ui-sref="turma.edit({id: {{$value.id}} })"><i class="glyphicon glyphicon-pencil"></i></a></span>'
+            }]
+        };
+
     };
-
-  };
-  return TurmaListController;
+    return TurmaListController;
 });
